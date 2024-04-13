c6ba1127615c2691e50938fcca29d1cb41bcdb8d
Nikita Sheremeta <guamoko95@gmail.com>
2024-04-13 14:00:15 +0300

    true error handling

---
+++ errors.go
@@ -0,0 +1,26 @@
+package calcbuilder
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidToken                       = errors.New("invalid token")
+	ErrUnexpectedEndOfExpression          = errors.New("anexpected end of expression")
+	ErrUnexpectedContinuationOfExpression = errors.New("unexpected continuation of expression")
+)
+
+// PositionErr wraps the error and stores the problematic position in the expression.
+type PositionErr struct {
+	Position   int
+	WrappedErr error
+}
+
+func (err PositionErr) Unwrap() error {
+	return err.WrappedErr
+}
+
+func (err PositionErr) Error() string {
+	return fmt.Sprintf("%s at position %d", err.WrappedErr.Error(), err.Position)
+}
